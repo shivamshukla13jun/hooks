023f5aa5c16e3bba752cfe50a21b996bf6b9fda4
shivam-handsintechnology <shivam@handsintechnology>
2024-11-26 19:25:29 +0530

    xz

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import useMultiStepForm from "./hooks/UseMultiStepForm"
-import Pagination from "./hooks/UsePagination"
-export { Pagination, useMultiStepForm }
+
+// src/index.js
+// export { default as Button } from './components/Button';
+// export { default as Input } from './components/Input';
+export { default as Pagination } from './hooks/UsePagination';
+export { default as useMultiStepForm } from './hooks/UseMultiStepForm';
+// export { default as useToast } from './hooks/useToast';
+// export { default as axiosInstance } from './utils/axiosInstance';
+// export { default as ThemeProvider } from './themes/ThemeProvider';
